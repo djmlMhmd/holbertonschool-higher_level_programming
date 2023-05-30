80b852e1de13ff5ffa5c9f998a051eef3f5e8ea6
djamalMhmd <djamaldine-mohamed@live.fr>
2023-05-30 18:23:01 +0200

    update code

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    upper_word = ""
-    for i in range(len(str)):
+
+    for i in range(0, len(str)):
         if 97 <= ord(str[i]) <= 122:
             upper_word += chr(ord('A') + (ord(str[i]) - ord('a')))
         else:
-            print("{:c}".format(ord(str[i]) - 32), end="")
+            print("{:c}".format(ord(str[i]) - 0), end="")
     print()
