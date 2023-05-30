f834eb1b47176ce918698635cb932013e3778b2e
djamalMhmd <djamaldine-mohamed@live.fr>
2023-05-30 15:59:59 +0200

    program prints possible different combinations

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for num in range(0, 10):
+    for i in range(num+1, 10):
+        if num == 8 and num == 8:
+            print("{}{}".format(num, i))
+        else:
+            print("{}{}".format(num, i), end=", ")
