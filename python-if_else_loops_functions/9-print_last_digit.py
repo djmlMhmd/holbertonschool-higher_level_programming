a0819610063c3caebf4417ef65cea99b1536b789
djamalMhmd <djamaldine-mohamed@live.fr>
2023-05-31 09:29:55 +0200

    add modulo 10

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
 
-    return number - (10 * int(number / 10))
+    last_digit = abs(number) % 10
+    return -last_digit if (number < 0) else last_digit
