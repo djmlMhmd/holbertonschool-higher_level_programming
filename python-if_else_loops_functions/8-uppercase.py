98f082ba960b003ff4291659f9d5af1f960d4441
djamalMhmd <djamaldine-mohamed@live.fr>
2023-05-31 09:09:57 +0200

    update code

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,8 +4,8 @@ def uppercase(str):
 
     for i in range(0, len(str)):
         if 97 <= ord(str[i]) <= 122:
-            i = 32
+            j = 32
         else:
-            i = 0
-        print("{:c}".format(ord(str[i]) - 0), end="")
+            j = 0
+        print("{:c}".format(ord(str[i]) - j), end="")
     print()
