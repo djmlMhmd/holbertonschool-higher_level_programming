77f192fd0198eaa240b763efe8f66254f801a154
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-07 15:45:26 +0200

    new code

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    new = [[i for i in set_1 if i in set_2]]
-    return(new)
+    new = []
+    for i in set_1:
+        if i in set_2:
+            new.append(i)
+    return new
