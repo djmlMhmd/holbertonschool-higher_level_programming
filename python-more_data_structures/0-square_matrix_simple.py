5429bc40a23a79579352c0b33877c3d0329ac268
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-07 15:44:37 +0200

    function that returns a set of common elements

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-   new_matrix = [[element ** 2 for element in rank] for rank in matrix]
-   return(new_matrix)
+    new_matrix = []
+    for rank in matrix:
+        new_rank = []
+        for element in rank:
+            new_rank.append(element ** 2)
+        new_matrix.append(new_rank)
+    return new_matrix
