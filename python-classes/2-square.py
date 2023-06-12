d3dcc51c78959c72545c7a9f0a9ed91efaaf7634
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-12 19:24:18 +0200

    change comment

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,7 +5,13 @@
 class Square:
     """Class of square"""
     def __init__(self, size=0):
-        """New square with his size"""
+        """New square with his size
+        Args :
+            size : Size of the square
+        Raises:
+            TypeError : If size not int
+            ValueError : If size is less than 0
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
