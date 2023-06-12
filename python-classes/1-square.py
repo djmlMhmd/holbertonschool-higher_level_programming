04871734ccae0f60a1d7c288ab66570eb44a73cc
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-12 18:48:38 +0200

    def size square

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define new class:"""
+
+
+class Square:
+    """A simple example class"""
+    def __init__(self, size=0):
+        """New square with his size"""
+        self.size = size
