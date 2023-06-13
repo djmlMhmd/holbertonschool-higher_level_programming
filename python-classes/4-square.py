903c8aedc699873428a49e619c0e6499f0230379
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-13 18:53:26 +0200

    change my codes

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -26,7 +26,7 @@ class Square:
     @property
     def size(self):
         """Returns the size of the square."""
-        return self.__size
+        return self.size
 
     @size.setter
     def size(self, value):
