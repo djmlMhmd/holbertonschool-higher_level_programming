0f5e0728f17a2ee9d3446d5e08726fa0d2562ee4
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-13 18:57:43 +0200

    remove useless function

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,16 +8,8 @@ class Square:
         """New square with his size
         Args :
             size : Size of the square
-        Raises:
-            TypeError : If size not int
-            ValueError : If size is less than 0
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
         """Returns : area of the square"""
