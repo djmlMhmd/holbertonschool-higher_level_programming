f2d90f3fa1c02018c72d820c0afe495824e1d3ac
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-15 13:58:53 +0200

    update

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -2,3 +2,16 @@
 def text_indentation(text):
     if not isinstance (text, str):
         raise TypeError("text must be a string")
+    punctuation_marks = ['.', '?', ':']
+    lines = []
+    result = ''
+
+    for char in text:
+        result += char
+        if char in punctuation_marks:
+            lines.append(result.strip())
+            result = ''
+    lines.append(result.strip())
+    for line in lines:
+        print(line)
+        print()
