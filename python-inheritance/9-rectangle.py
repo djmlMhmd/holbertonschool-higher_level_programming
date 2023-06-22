049b5ffa8bb24dbfdff122d461bace326d75cc42
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-22 12:22:55 +0200

    update

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -2,15 +2,8 @@
 """Create class BaseGeometry"""
 
 
-class BaseGeometry:
-    def area(self):
-        return self.__width * self.__height
-    
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
-        elif value <= 0:
-            raise ValueError("<name> must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """Class Rectangle, subclass of BaseGeometry
@@ -20,13 +13,15 @@ class Rectangle(BaseGeometry):
         height (int): height of the rectangle
     """
     def __init__(self, width, height):
-        super().__init__()
-        self.width = 0
-        self.height = 0
-        self.width = width
-        self.height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """Using BaseGeometry function"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"
+    
+    def area(self):
+        """Return the area"""
+        return self.__width * self.__height
