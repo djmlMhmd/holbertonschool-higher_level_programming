0559ed7f461bc541b379c3a4f1d983464044afad
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-22 11:16:02 +0200

    update

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,5 +12,5 @@ class BaseGeometry():
         """Validates value"""
         if not isinstance(value, int):
             raise TypeError("<name> must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("<name> must be greater than 0")
