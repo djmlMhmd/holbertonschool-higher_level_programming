82bda68a4ee6b03aa07e0946c275c1e815722c8c
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-22 12:25:27 +0200

    remove useless line

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"
-    
+
     def area(self):
         """Return the area"""
         return self.__width * self.__height
