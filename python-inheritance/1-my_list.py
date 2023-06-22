f10271f91b6c878a5d364a754571f6c1674791bb
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-22 10:17:35 +0200

    add description

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+
+"""
+class MyList that inherits from list
+"""
+
 class MyList(list):
+
+    """
+    A custom list class.
+    """
+
     def print_sorted(self):
+
+        """
+        Print the list in sorted order.
+        """
+
         self.sort
         print(self)
