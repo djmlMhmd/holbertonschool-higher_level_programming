978c7157073d0e496fbd0b5aa61db9716ca210eb
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-22 10:52:26 +0200

    add my function

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""same class or inherit from"""
+
+
 def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class that inherited
+    from the specified class ; otherwise False."""
     if type(obj) is a_class:
         return False
     else:
