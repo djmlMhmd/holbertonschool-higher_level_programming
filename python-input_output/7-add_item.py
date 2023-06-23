fcb4700e79e7ebe8c45351e05b6fd8b2d2ceff6d
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-23 23:35:28 +0200

    add new line

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,6 +2,8 @@
 """script that adds all arguments"""
 import sys
 import os
+
+
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
