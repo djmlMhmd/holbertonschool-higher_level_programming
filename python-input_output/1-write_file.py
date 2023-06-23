f1ff4c8f679089106c4e5d1e28196ec4527e85d1
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-23 23:33:41 +0200

    add new line

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,7 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    """writes a string to a text file (UTF8) and returns the number of characters written"""
+    """writes a string to a text file"""
     with open(filename, "w", encoding="utf-8") as file:
         with open(filename, "w", encoding='utf-8') as f:
             return f.write(text)
