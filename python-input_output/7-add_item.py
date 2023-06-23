5642008311cf74ddc69d1f907d7a205bdf79bb56
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-23 23:27:02 +0200

    update

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,12 +2,17 @@
 """script that adds all arguments"""
 import sys
 import os
-save_to_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+args = sys.argv
 filename = "add_item.json"
 
-data = load_from_json_file(filename) if os.path.exists(filename) else []
-data.extend(sys.argv[1:])
+if os.path.isfile(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
 
-save_to_file(data, filename)
+my_list.extend(args[1:])
+
+save_to_json_file(my_list, filename)
