61bed10e9e73ee50939e2d38f83abd691cb32de6
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-27 19:29:56 +0200

    adding the public method  that assignes attributes

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -22,3 +22,18 @@ class Square(Rectangle):
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            if kwargs:
+                for key, value in kwargs.items():
+                    setattr(self, key, value)
