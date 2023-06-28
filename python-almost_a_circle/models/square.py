499820b620e8d01004b0d77c515b4afc1ba0fd96
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-28 10:05:59 +0200

    assigns attribut

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -24,16 +24,16 @@ class Square(Rectangle):
             self.id, self.x, self.y, self.width)
 
     def update(self, *args, **kwargs):
-        if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.size = args[1]
-            if len(args) >= 3:
-                self.x = args[2]
-            if len(args) >= 4:
-                self.y = args[3]
+        """Assigns an argument"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.size = args[1]
+        if len(args) >= 3:
+            self.x = args[2]
+        if len(args) >= 4:
+            self.y = args[3]
         else:
-            if kwargs:
                 for key, value in kwargs.items():
+                    """assigns a value to an object attribute"""
                     setattr(self, key, value)
