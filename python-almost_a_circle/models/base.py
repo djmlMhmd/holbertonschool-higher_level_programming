8531fcc63ba019e75b9be9b8af7a985db911ccb2
djamalMhmd <djamaldine-mohamed@live.fr>
2023-06-29 10:17:07 +0200

    change save to file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,11 +24,10 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """Writes JSON string representation of list_objs to a file"""
-        filename = cls.__name__ + ".json"
-        json_string = cls.to_json_string(
-            [obj.to_dictionary() for obj in list_objs])
-        with open(filename, 'w') as file:
-            file.write(json_string)
+        filename = type(list_objs[0]).__name__ + ".json"
+        json_file = open(filename, "w")
+        if list_objs is None:
+            json.dump([], json_file)
 
     @staticmethod
     def from_json_string(json_string):
@@ -36,3 +35,4 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
