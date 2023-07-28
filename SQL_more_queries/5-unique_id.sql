a3e29d03a77e12adfae18398e3abf1de44618c40
djamalMhmd <djamaldine-mohamed@live.fr>
2023-07-28 10:56:30 +0200

    adding exercices

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- Script that creates the table unique_id on my MySQL server.
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256)
+) AUTO_INCREMENT=1;
