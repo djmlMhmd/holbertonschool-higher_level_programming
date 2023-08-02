321f3cba617f71e34c8c4f0f3ace97a11713df21
djamalMhmd <djamaldine-mohamed@live.fr>
2023-08-02 12:31:23 +0200

    adding requirement

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Script that lists all states with a name starting with N 
+"""Script that lists all states with a name starting with N
 (upper N) from the database hbtn_0e_0_usa"""
 
 import sys
@@ -19,11 +19,11 @@ if __name__== "__main__":
         """
         SELECT * FROM states
         WHERE name LIKE BINARY 'N%' 
-        ORDER BY states.id
-        """)
+        ORDER BY states.id""")
 
     rows = cur.fetchall()
     for row in rows:
         print(row)
+
     cur.close()
     db.close()
