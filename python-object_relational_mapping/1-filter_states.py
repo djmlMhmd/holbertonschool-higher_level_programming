b47a45d68a89d7a2621aaa9bf0eff8f4d8bf50bc
djamalMhmd <djamaldine-mohamed@live.fr>
2023-08-02 12:22:43 +0200

    correction

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+"""Script that lists all states with a name starting with N 
+(upper N) from the database hbtn_0e_0_usa"""
 
 import sys
 import MySQLdb
@@ -10,9 +11,10 @@ if __name__== "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3])
+        db=sys.argv[3]
+        )
+    
     cur = db.cursor()
-
     cur.execute("""SELECT * FROM states
                 WHERE name LIKE BINARY 'N%' 
                 ORDER BY states.id""")
