5938536ad3817b891e19462376bebe0a52de116e
djamalMhmd <djamaldine-mohamed@live.fr>
2023-08-02 17:15:22 +0200

    fixe checker

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,7 +9,9 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
-    """An object representation of the table States in our database"""
+    """
+    An object representation of the table States in our database
+    """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
